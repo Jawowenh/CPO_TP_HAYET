31fb7e9e1a92a8cc35de71fb1e627bedc30bc61a
owenh <owen.hayet@epfedu.fr>
2023-09-25 11:11:54 +0200

    TP0-Exercice 1

---
+++ TP0_Hayet/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,30 @@
+/*
+ * Nom, Prenom: Hayet Owen
+ *Groupe:TDC
+ *Date: Septembre 2023
+ *Role: exercice1
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author owenh
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Salut, tu t'appelle comment?");
+        sc.nextLine();
+    }
+    
+}
