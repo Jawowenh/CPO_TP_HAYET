c5faa235ba8e4533b10552796039097121fe762b
owenh <owen.hayet@epfedu.fr>
2023-10-01 14:58:43 +0200

    TP0-Exercice1,2, calculator

--- TP0_Hayet/Calculator/src/calculator/Calculator.java
+++ TP0_Hayet/Calculator/src/calculator/Calculator.java
@@ -28,13 +28,29 @@ public class Calculator {
                            """);
         int operateur, operande1, operande2; 
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n Pleace choose the foirst number :");
+        operateur= sc.nextInt();
+        if ( operateur<1 || operateur>5){
+            System.out.println ("Please choose an other number ");
+            System.exit(0);}
+        
+       System.out.println("\n Pleace choose the first number :");
         operande1 = sc.nextInt(); 
+        System.out.println("\n Pleace choose the second number :");
+        operande2 = sc.nextInt(); 
+         
+        if (operateur == 1) {
+            System.out.println(operande1 + operande2);}
+        if (operateur == 2) {
+            System.out.println(operande1 - operande2);}
+        if (operateur == 3) {
+            System.out.println(operande1 * operande2);}
+        if (operateur == 4) {
+            System.out.println(operande1 / operande2);}
+        if (operateur == 5) {
+            System.out.println(operande1 % operande2);}
+            
+                    
         
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\n please choose the second number :");
-        operande2 = sc.nextInt();
-        
-    }
+        }}
     
-}
+
