640dce0dcb9acbae29b87956f21de8d90ba0e384
owenh <owen.hayet@epfedu.fr>
2023-10-13 18:13:39 +0200

    Tp2 biere

---
+++ New Folder (3)/TP2_Bieres_HAYE/src/tp2_bieres_haye/BouteilleBiere.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_haye;
+
+/**
+ *
+ * @author owenh
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+   public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+
+}
+public BouteilleBiere(String unNom, double unDegre, String uneBrasserie, boolean OUVERTE) {
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = OUVERTE;
+}
+public boolean decapsuler(){
+       if (ouverte!=true) {
+          ouverte=true;
+          return true;}
+       else {
+         System.out.println("Erreur : la bière est ouverte.");
+        return false;
+       }
+}
+@Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degrés)Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;} }   
